d002fbfe913c68d7e09622071f9d4646ddab8203
rptr <4176588+rptr@users.noreply.github.com>
2026-04-06 22:57:16 +0300

    test.sh: print pass/fail green/red

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <path>"
@@ -8,6 +10,9 @@ if [ -z "$1" ]; then
 fi
 
 find "$1" -type f | sort | while read -r file; do
-    echo "Testing: $file"
-    ./target/debug/rticc "$file"
+    if ./target/debug/rticc "$file"; then
+        echo -e "${GREEN}PASS${NC}: $file"
+    else
+        echo -e "${RED}FAIL${NC}: $file"
+    fi
 done
